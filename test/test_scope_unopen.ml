c06baea934b6d6eea4003174f6b176b44cdacfa8
Valentin Gatien-Baron <valentin.gatienbaron@gmail.com>
2025-02-07 14:26:45 +0100

    test: refactor test_scope_unopen

--- test/test_scope_unopen.ml
+++ test/test_scope_unopen.ml
@@ -1,22 +1,36 @@
 let test = Test_migration.test
 
 let () =
-  test "unopen"
+  test "value"
     (module struct
-      type t = int
-
       let z2 = 1
 
       open Z
 
-      let _ = (z2, z1, Nested.n1, (1 : z), (1 : t))
+      let _ = (z2, z1, Nested.n1)
     end)
 [@@migrate_test.unopen
   let () =
-    test "unopen"
+    test "value"
+      (module struct
+        let z2 = 1
+        let _ = (z2, Z.z1, Z.Nested.n1)
+      end)]
+
+let () =
+  test "type"
+    (module struct
+      type t = int
+
+      open Z
+
+      let _ = ((1 : z), (1 : t))
+    end)
+[@@migrate_test.unopen
+  let () =
+    test "type"
       (module struct
         type t = int
 
-        let z2 = 1
-        let _ = (z2, Z.z1, Z.Nested.n1, (1 : Z.z), (1 : t))
+        let _ = ((1 : Z.z), (1 : t))
       end)]
