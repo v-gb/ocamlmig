c2f46846dc170bc3c79880da589162961c960460
Valentin Gatien-Baron <valentin.gatienbaron@gmail.com>
2025-02-10 15:44:01 +0100

    preserve_format: add a test showing missing parens in rewrite

--- test/test_preserve_format.ml
+++ test/test_preserve_format.ml
@@ -1,6 +1,6 @@
 [@@@ocamlformat "disable"]
 
-(* comment 1 *)
+(* comments should be preserved *)
   let _ =   0   + Fun.id
       ((* comment 2 *) 1
       +
@@ -8,15 +8,24 @@
       2 (* comment 4 *))
 (* comment 5 *)
 
+(* parens should be inserted where necessary, not currently implemented *)
+
+let _ = ignore "hello"
+
 (* Result:
 
 [@@@ocamlformat "disable"]
 
-(* comment 1 *)
+(* comments should be preserved *)
   let _ =   0   + (* comment 2 *) 1
                   +
                   (* comment 3 *)
                   2 (* comment 4 *)
 (* comment 5 *)
+
+(* parens should be inserted where necessary *)
+
+let _ = ignore ();
+               "hello"
 
 *)
