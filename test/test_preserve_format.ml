bd5f5079bef7fd42efc13c8389dc446a524257b0
Valentin Gatien-Baron <valentin.gatienbaron@gmail.com>
2025-02-10 15:31:08 +0100

    make the preserve-format test show it preserves wacky formatting

--- test/test_preserve_format.ml
+++ test/test_preserve_format.ml
@@ -1,20 +1,22 @@
+[@@@ocamlformat "disable"]
+
 (* comment 1 *)
-let _ =
-  Fun.id
-    ((* comment 2 *) 1
-    +
-    (* comment 3 *)
-    2 (* comment 4 *))
+  let _ =   0   + Fun.id
+      ((* comment 2 *) 1
+      +
+      (* comment 3 *)
+      2 (* comment 4 *))
 (* comment 5 *)
 
 (* Result:
 
+[@@@ocamlformat "disable"]
+
 (* comment 1 *)
-let _ =
-  (* comment 2 *) 1
-  +
-  (* comment 3 *)
-  2 (* comment 4 *)
+  let _ =   0   + (* comment 2 *) 1
+                  +
+                  (* comment 3 *)
+                  2 (* comment 4 *)
 (* comment 5 *)
 
 *)
