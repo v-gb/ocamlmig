111618785bc1568c0a2608e808c157cd9aa289fb
Valentin Gatien-Baron <valentin.gatienbaron@gmail.com>
2025-02-24 19:20:58 +0100

    preserve_format: add tests for tricky case

--- test/test_preserve_format_unopen.ml
+++ test/test_preserve_format_unopen.ml
@@ -5,6 +5,12 @@ open Format
 
 let  _  = print_string
 
+(* A couple of tricky cases involving parsing changing behavior when removing
+   a structure item. The first case actually causing an internal exception, so
+   it has to be commented out. *)
+(* include struct let () = (); open! Format let () = () end *)
+include struct let () = ();open! Format;;1 end
+
 (* Result:
 
 (* comment *)
@@ -12,5 +18,11 @@ let  _  = print_string
 
 
 let  _  = Format.print_string
+
+(* A couple of tricky cases involving parsing changing behavior when removing
+   a structure item. The first case actually causing an internal exception, so
+   it has to be commented out. *)
+(* include struct let () = (); open! Format let () = () end *)
+include struct let () = (); ;;1 end
 
 *)
