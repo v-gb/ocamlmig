cce149cce73b5895ccda83d0c49632a6e9e3e4d5
Valentin Gatien-Baron <valentin.gatienbaron@gmail.com>
2025-05-19 10:55:18 +0200

    add minimal functions to convert between asts without printing/parsing

--- lib/conv.ml
+++ lib/conv.ml
@@ -1,34 +1,107 @@
+open Base
+module P = Parsetree
+
 module Fmast = struct
   include Ocamlformat_ocaml_common
   include Ocamlformat_parser_extended
+  module P = Parsetree
 end
 
-let location : Location.t -> Ocamlformat_ocaml_common.Location.t =
+let location : Location.t -> Fmast.Location.t =
  fun loc ->
   { loc_start = loc.loc_start; loc_end = loc.loc_end; loc_ghost = loc.loc_ghost }
 
-let location' : Ocamlformat_ocaml_common.Location.t -> Location.t =
+let location' : Fmast.Location.t -> Location.t =
  fun loc ->
   { loc_start = loc.loc_start; loc_end = loc.loc_end; loc_ghost = loc.loc_ghost }
 
-let located conv_a : _ Location.loc -> _ Ocamlformat_ocaml_common.Location.loc =
+let located conv_a : _ Location.loc -> _ Fmast.Location.loc =
  fun { txt; loc } -> { txt = conv_a txt; loc = location loc }
 
-let located' conv_a : _ Ocamlformat_ocaml_common.Location.loc -> _ Location.loc =
+let located' conv_a : _ Fmast.Location.loc -> _ Location.loc =
  fun { txt; loc } -> { txt = conv_a txt; loc = location' loc }
 
-let rec longident' : Ocamlformat_ocaml_common.Longident.t -> Longident.t = function
+let rec longident' : Fmast.Longident.t -> Longident.t = function
   | Lident s -> Lident s
   | Ldot (t, s) -> Ldot (longident' t, s)
   | Lapply (t1, t2) -> Lapply (longident' t1, longident' t2)
 
-let rec longident : Longident.t -> Ocamlformat_ocaml_common.Longident.t = function
+let rec longident : Longident.t -> Fmast.Longident.t = function
   | Lident s -> Lident s
   | Ldot (t, s) -> Ldot (longident t, s)
   | Lapply (t1, t2) -> Lapply (longident t1, longident t2)
 
-let arg_label : Asttypes.arg_label -> Ocamlformat_parser_extended.Asttypes.arg_label =
-  function
+let arg_label : Asttypes.arg_label -> Fmast.Asttypes.arg_label = function
   | Nolabel -> Nolabel
   | Labelled s -> Labelled { txt = s; loc = Fmast.Location.none }
   | Optional s -> Optional { txt = s; loc = Fmast.Location.none }
+
+let rec expr : P.expression -> Fmast.P.expression =
+ fun { pexp_desc; pexp_loc; pexp_loc_stack; pexp_attributes } ->
+  { pexp_desc = expr_desc pexp_desc
+  ; pexp_loc = location pexp_loc
+  ; pexp_loc_stack = List.map ~f:location pexp_loc_stack
+  ; pexp_attributes = attributes pexp_attributes
+  }
+
+and expr_desc : P.expression_desc -> Fmast.P.expression_desc = function
+  | Pexp_ident ident -> Pexp_ident (located longident ident)
+  | _ -> raise Stdlib.Exit
+
+and attributes : P.attributes -> Fmast.P.attributes = fun l -> List.map l ~f:attribute
+
+and attribute : P.attribute -> Fmast.P.attribute =
+ fun { attr_name; attr_payload; attr_loc } ->
+  { attr_name = located Fn.id attr_name
+  ; attr_payload = payload attr_payload
+  ; attr_loc = location attr_loc
+  }
+
+and payload : P.payload -> Fmast.P.payload = function
+  | PStr l -> PStr (structure l)
+  | _ -> raise Stdlib.Exit
+
+and structure : P.structure -> Fmast.P.structure = fun l -> List.map l ~f:structure_item
+
+and structure_item : P.structure_item -> Fmast.P.structure_item =
+ fun { pstr_desc; pstr_loc } ->
+  { pstr_desc = structure_item_desc pstr_desc; pstr_loc = location pstr_loc }
+
+and structure_item_desc : P.structure_item_desc -> Fmast.P.structure_item_desc = function
+  | Pstr_eval (e, attrs) -> Pstr_eval (expr e, attributes attrs)
+  | _ -> raise Stdlib.Exit
+
+let rec expr' : Fmast.P.expression -> P.expression =
+ fun { pexp_desc; pexp_loc; pexp_loc_stack; pexp_attributes } ->
+  { pexp_desc = expr_desc' pexp_desc
+  ; pexp_loc = location' pexp_loc
+  ; pexp_loc_stack = List.map ~f:location' pexp_loc_stack
+  ; pexp_attributes = attributes' pexp_attributes
+  }
+
+and expr_desc' : Fmast.P.expression_desc -> P.expression_desc = function
+  | Pexp_ident ident -> Pexp_ident (located' longident' ident)
+  | _ -> raise Stdlib.Exit
+
+and attributes' : Fmast.P.attributes -> P.attributes = fun l -> List.map l ~f:attribute'
+
+and attribute' : Fmast.P.attribute -> P.attribute =
+ fun { attr_name; attr_payload; attr_loc } ->
+  { attr_name = located' Fn.id attr_name
+  ; attr_payload = payload' attr_payload
+  ; attr_loc = location' attr_loc
+  }
+
+and payload' : Fmast.P.payload -> P.payload = function
+  | PStr l -> PStr (structure' l)
+  | _ -> raise Stdlib.Exit
+
+and structure' : Fmast.P.structure -> P.structure = fun l -> List.map l ~f:structure_item'
+
+and structure_item' : Fmast.P.structure_item -> P.structure_item =
+ fun { pstr_desc; pstr_loc } ->
+  { pstr_desc = structure_item_desc' pstr_desc; pstr_loc = location' pstr_loc }
+
+and structure_item_desc' : Fmast.P.structure_item_desc -> P.structure_item_desc = function
+  | Pstr_eval (e, attrs) -> Pstr_eval (expr' e, attributes' attrs)
+  | _ -> raise Stdlib.Exit
--- lib/conv.mli
+++ lib/conv.mli
@@ -1,9 +1,12 @@
 (** Various functions that copy one type in the upstream ast to the same type in the
     ocamlformat ast, and vice versa. *)
 
+module P = Parsetree
+
 module Fmast : sig
   include module type of Ocamlformat_ocaml_common
   include module type of Ocamlformat_parser_extended
+  module P = Parsetree
 end
 
 val location : Location.t -> Fmast.Location.t
@@ -13,3 +16,15 @@ val located' : ('a -> 'b) -> 'a Fmast.Location.loc -> 'b Location.loc
 val longident' : Fmast.Longident.t -> Longident.t
 val longident : Longident.t -> Fmast.Longident.t
 val arg_label : Asttypes.arg_label -> Fmast.Asttypes.arg_label
+
+(** These ast converting functions used to be implemented as simply print/parse, which is
+    much easier. The problem is that locations are lost when doing this, which is not easy
+    to improve. And we need locations to match between asts when we lookup fmast locations
+    in a Build.Type_index.t, which is built from a typedtree, which itself is built from
+    an uast, which may either come from parsing the same string as for creating the fmast
+    (in which case, everything is good either method), or by conversion of the uast into
+    fmast (as happens when typing repl expressions on the fly, in which case the loss of
+    location means types are unfindable). *)
+
+val expr : P.expression -> Fmast.P.expression
+val expr' : Fmast.P.expression -> P.expression
